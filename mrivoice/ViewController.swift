c6df191c20e64412f5ab69d1569fae4f33356f99
it_dev01 <it_dev01@macnote-12.local>
2018-03-20 09:41:55 +0900

    Initial Commit

---
+++ mrivoice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mrivoice
+//
+//  Created by it_dev01(yamao) on 2018/03/20.
+//  Copyright © 2018年 it_dev01(yamao). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
